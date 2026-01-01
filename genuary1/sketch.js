fd4bb8483a1c7b6ee40a7cc1bb69ecc9b576eff3
Revans <revanspr@gmail.com>
2026-01-01 13:46:14 -0500

    genuary1

---
+++ genuary1/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(255);
+}
+
+function draw() {
+    // Your genuary1 code here
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
